153a659887ace4ae8cc10de52b09778e7a1e667e
stefanos-larkou <s.larkou@outlook.com>
2026-08-20 18:29:10 +0300

    veil a hex border along with its face

--- src/render/draw.ts
+++ src/render/draw.ts
@@ -38,4 +38,9 @@ function drawHex(context: CanvasRenderingContext2D, hex: DrawnHex): void {
 
     context.strokeStyle = hex.style.stroke;
     context.stroke();
+
+    if (hex.veil) {
+        context.strokeStyle = hex.veil;
+        context.stroke();
+    }
 }
